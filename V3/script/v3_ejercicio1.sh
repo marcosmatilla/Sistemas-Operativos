5efa0ec8d6b8cbfd7e0caade333648b27d92c611
UO258935 <UO258935@uniovi.es>
2020-04-13 15:44:53 +0200

    Revert "c"
    This reverts commit 085dc9e6fc0c3775ec9989794c2c624e317f7d2e.

--- V3/script/v3_ejercicio1.sh
+++
@@ -1,7 +0,0 @@
-#bin/bash.rc
-cp programVerySimple ../programVerySimple
-cd ..
-make clean
-make
-./Simulator --debugSections= 0 programVerySimple 3 programVerySimple 1 programVerySimple
-cd scriptsejercicios/
